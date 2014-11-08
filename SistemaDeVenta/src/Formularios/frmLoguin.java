331cd290ba7844ad552af7a0194be09cf77b9522
pabloalfredo <pabloalfredo@pabloalfredo-PC>
2014-11-08 11:23:37 -0800

    frmLoguin

---
+++ SistemaDeVenta/src/Formularios/frmLoguin.java
@@ -0,0 +1,133 @@
+package Formularios;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
+import java.awt.SystemColor;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Window.Type;
+import java.awt.Color;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class frmLoguin {
+
+	private JFrame frame;
+	private JTextField txtUsuario;
+	private JPasswordField txtpass;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frmLoguin window = new frmLoguin();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public frmLoguin() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(0, 153, 153));
+		frame.setBackground(new Color(0, 153, 153));
+		frame.setType(Type.UTILITY);
+		frame.getContentPane().setFont(new Font("Tahoma", Font.BOLD, 12));
+		frame.setBounds(100, 100, 375, 210);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblControlDeUsuario = new JLabel("Control de Usuario");
+		lblControlDeUsuario.setForeground(Color.WHITE);
+		lblControlDeUsuario.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setForeground(Color.WHITE);
+		lblUsuario.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JLabel lblContrasena = new JLabel("Contrasena");
+		lblContrasena.setForeground(Color.WHITE);
+		lblContrasena.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.setFont(new Font("Tahoma", Font.BOLD, 12));
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBackground(SystemColor.activeCaption);
+		btnCancelar.setFont(new Font("Tahoma", Font.BOLD, 12));
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setColumns(10);
+		
+		txtpass = new JPasswordField();
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(132)
+							.addComponent(lblControlDeUsuario))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(80)
+							.addComponent(lblUsuario)
+							.addGap(21)
+							.addComponent(txtUsuario, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(63)
+							.addComponent(lblContrasena)
+							.addGap(10)
+							.addComponent(txtpass, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(154)
+							.addComponent(btnAceptar, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)))
+					.addGap(30))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(11)
+					.addComponent(lblControlDeUsuario, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
+					.addGap(24)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(1)
+							.addComponent(lblUsuario, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtUsuario, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(13)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(1)
+							.addComponent(lblContrasena, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtpass, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnAceptar, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)))
+		);
+		frame.getContentPane().setLayout(groupLayout);
+	}
+}
