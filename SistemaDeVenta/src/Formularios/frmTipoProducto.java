c9ddcb1aa21522cf61a89ddc3aee9a3e64d13b6b
HP User <HP User@HPUser-PC.lan>
2014-11-08 14:01:51 -0600

    boton

--- SistemaDeVenta/src/Formularios/frmTipoProducto.java
+++ SistemaDeVenta/src/Formularios/frmTipoProducto.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class frmTipoProducto extends JFrame {
 
@@ -35,8 +36,12 @@ public class frmTipoProducto extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(147, 95, 107, 79);
+		contentPane.add(btnNewButton);
 	}
 
 }
