2a10e6b837d2c19771a27717e61fd5ed9dc993a1
pabloalfredo <pabloalfredo@pabloalfredo-PC>
2014-11-08 09:44:03 -0800

    otra cosa

--- SistemaDeVenta/src/Formularios/frmPrincipal.java
+++ SistemaDeVenta/src/Formularios/frmPrincipal.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class frmPrincipal {
 
@@ -52,6 +53,9 @@ public class frmPrincipal {
 		
 		JMenu mnConsultas = new JMenu("Consultas");
 		menuBar.add(mnConsultas);
+		
+		JMenuItem mntmE = new JMenuItem("e");
+		mnConsultas.add(mntmE);
 	}
 
 }
