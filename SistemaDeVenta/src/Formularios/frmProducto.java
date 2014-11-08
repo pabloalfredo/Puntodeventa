f543997d1f522fbd0b0d226874006b6c3e2cfc85
vladimir <vladimir@vladimir-PC>
2014-11-08 14:51:18 -0400

    Menu en Producto

--- SistemaDeVenta/src/Formularios/frmProducto.java
+++ SistemaDeVenta/src/Formularios/frmProducto.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class frmProducto extends JFrame {
 
@@ -33,6 +38,20 @@ public class frmProducto extends JFrame {
 	public frmProducto() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu menu = new JMenu("New menu");
+		menuBar.add(menu);
+		
+		JMenuItem mntmAbrir = new JMenuItem("Abrir");
+		mntmAbrir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		menu.add(mntmAbrir);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
