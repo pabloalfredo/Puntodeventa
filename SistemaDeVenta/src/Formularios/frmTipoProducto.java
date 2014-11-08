c75a56de539af2a7f95fb242a32df23133c178b1
vladimir <vladimir@vladimir-PC>
2014-11-08 14:15:40 -0400

    agreagado boton

--- SistemaDeVenta/src/Formularios/frmTipoProducto.java
+++ SistemaDeVenta/src/Formularios/frmTipoProducto.java
@@ -6,10 +6,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class frmTipoProducto extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -35,8 +38,17 @@ public class frmTipoProducto extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(39, 96, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(28, 53, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
 	}
 
 }
